428f737d0bfc5fec20357715f8a2b483ed0b2315
Anastasia Mironova <anastasia.mironova7@gmail.com>
2023-02-10 07:12:50 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ program
 .version('0.0.1', '-V, --version', 'output the version number')
 .description('Compares two configuration files and shows a difference')
 
+.option('-f, --format <type>', 'output format')
+.argument('filepath1')
+.argument('filepath2')
 
 
 
