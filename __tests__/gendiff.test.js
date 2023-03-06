9f212a37d3765a6f9833965453493e55954aea59
Anastasia Mironova <anastasia.mironova7@gmail.com>
2023-03-06 22:09:56 +0300

    changes in fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 import nestedDiff from '../__fixtures__/nestedDiff.js';
 import plainDiff from '../__fixtures__/plainDiff.js';
-// import jsonDiff from '../__fixtures__/jsonDiff.json';
+
 const jsonDiff = JSON.stringify(JSON.parse(fs.readFileSync('__fixtures__/jsonDiff.json', 'utf8')));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
